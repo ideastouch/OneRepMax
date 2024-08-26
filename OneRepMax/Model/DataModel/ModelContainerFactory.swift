181fba4e203f740c5bc76ccb8e6be57885c226c1
ideastouch <gustavoh@ideastouch.com>
2024-08-25 22:02:19 -0700

    Since we don't need anymore ModelContainerProxy because we hold the ModelContainer in the AppManager MainActor , this class was upgrade to an actor that just create one model container.

---
+++ OneRepMax/Model/DataModel/ModelContainerFactory.swift
@@ -0,0 +1,23 @@
+//
+//  ModelContainerProxy.swift
+//  OneRepMax
+//
+//  Created by Gustavo Halperin on 6/20/24.
+//
+
+
+import Foundation
+import SwiftUI
+import SwiftData
+
+actor ModelContainerFactory {
+    func makeOne(isStoredInMemoryOnly: Bool = false) throws -> ModelContainer {
+        let schema = Schema([
+            Workout.self,
+            WorkoutHistorical.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema,
+                                                    isStoredInMemoryOnly: isStoredInMemoryOnly)
+        return try ModelContainer(for: schema, configurations: [modelConfiguration])
+    }
+}
